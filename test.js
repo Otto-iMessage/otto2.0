ea680707ac71de86796f2a516becfad9b08e99d0
Ryan Densmore <rdensmore365@gmail.com>
2017-12-15 21:59:34 -0500

    FINALLY GOT IT WORKING

--- test.js
+++ test.js
@@ -2,4 +2,7 @@ const osascript = require('node-osascript')
 
 let handle = 'rdensmore365@gmail.com'
 
-
+osascript.execute(`tell app "Contacts" to name of people where vcard contains "${handle}"`, (err, result, raw) => {
+  if (err) return console.error(err)
+  console.log(result[0])
+})
